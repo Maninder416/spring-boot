2c51d0f5618874069595d1b59e9649f59bb44f4b
Maninder416 <Maninder.singh@fullstacklabs.ca>
2023-03-28 16:55:57 -0400

    added maven tool in jenkins

--- src/main/java/io/reactivestax/springboot/repository/EmployeeRepository.java
+++ src/main/java/io/reactivestax/springboot/repository/EmployeeRepository.java
@@ -2,6 +2,7 @@ package io.reactivestax.springboot.repository;
 
 import io.reactivestax.springboot.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
