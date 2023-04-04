32cd8c95e1618719be0fd6f716076c26945ad7b6
Maninder416 <Maninder.singh@fullstacklabs.ca>
2023-04-04 13:58:10 -0400

    added next js folder

---
+++ nextjs-demo/src/pages/about.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const About: NextPage = () => {
+    return (
+        <div className={styles.container}>
+        <h1>Welcome to About page</h1>
+        </div>
+        )
+
+}
+
+export default About
