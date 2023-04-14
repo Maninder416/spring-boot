a27cf5f8a679c974a4669be5e644d9834bf8e468
Maninder416 <Maninder.singh@fullstacklabs.ca>
2023-04-14 11:58:02 -0400

    added security class

---
+++ src/main/java/io/reactivestax/config/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package io.reactivestax.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("maninder")
+                .password("maninder")
+                .roles("USER");
+
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
