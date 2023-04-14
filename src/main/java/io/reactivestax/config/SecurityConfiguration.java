1637dc191f4bb869a6b774cfc23405f2c7ff283d
Maninder416 <Maninder.singh@fullstacklabs.ca>
2023-04-14 17:19:34 -0400

    added all working code in which credentials are picked from db

--- src/main/java/io/reactivestax/config/SecurityConfiguration.java
+++ src/main/java/io/reactivestax/config/SecurityConfiguration.java
@@ -1,24 +1,65 @@
 package io.reactivestax.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+
+    /**
+     * Here we are taking the user credentials from db.
+     *
+     * @param auth
+     * @throws Exception
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("maninder")
-                .password("maninder")
-                .roles("USER");
+        auth.userDetailsService(userDetailsService);
 
     }
+
+    /**
+     * Below code is taking the credentials from in-memory
+     * we define all credential over the method
+     *
+     * @return
+     */
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("maninder")
+//                .password("maninder")
+//                .roles("USER")
+//                .and()
+//                .withUser("admin")
+//                .password("admin")
+//                .roles("ADMIN");
+//
+//    }
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
+                .and()
+                .formLogin();
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into user2 values(2,"maninder","maninder","ADMIN",true);
