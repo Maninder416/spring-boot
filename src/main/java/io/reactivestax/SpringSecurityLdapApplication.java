885346e15198a56d724a53497406cee24782c127
Maninder416 <Maninder.singh@fullstacklabs.ca>
2023-04-17 11:15:39 -0400

    added all working code in which credentials are picked from db

--- src/main/java/io/reactivestax/SpringSecurityLdapApplication.java
+++ src/main/java/io/reactivestax/SpringSecurityLdapApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAwsApplication {
+public class SpringSecurityLdapApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringAwsApplication.class, args);
+		SpringApplication.run(SpringSecurityLdapApplication.class, args);
 	}
 
 }
