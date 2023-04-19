4c6a417b354b4bbfc3180a0d5f1deda88cc4d89c
Maninder416 <Maninder.singh@fullstacklabs.ca>
2023-04-19 10:30:00 -0400

    added postman and doc

--- src/main/java/io/reactivestax/filters/JwtRequestFilter.java
+++ src/main/java/io/reactivestax/filters/JwtRequestFilter.java
@@ -24,7 +24,9 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     private JwtUtil jwtUtil;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
         final String authorizationHeader = request.getHeader("Authorization");
         String username = null;
         String jwt = null;
@@ -34,37 +36,13 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         }
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-            if (!jwtUtil.validateToken(jwt, userDetails)) {
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
+            if (jwtUtil.validateToken(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new
+                        UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
         filterChain.doFilter(request, response);
     }
-
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//        final String authorizationHeader= request.getHeader("Authorization");
-//        String username= null;
-//        String jwt=null;
-//
-//        if(authorizationHeader !=null &&authorizationHeader.startsWith("Bearer ")){
-//            jwt= authorizationHeader.substring(7);
-//            username= jwtUtil.extractUsername(jwt);
-//        }
-//
-//        if(username !=null && SecurityContextHolder.getContext().getAuthentication()==null){
-//            UserDetails userDetails= this.userDetailsService.loadUserByUsername(username);
-//            if(jwtUtil.validateToken(jwt,userDetails)){
-//                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken= new UsernamePasswordAuthenticationToken(userDetails,
-//                        null,userDetails.getAuthorities());
-//                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-//                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-//            }
-//        }
-//        filterChain.doFilter(request,response);
-//
-//    }
 }
