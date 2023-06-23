6d64458757810376f0e45098257166d64eca9f21
Maninder416 <Maninder.singh@fullstacklabs.ca>
2023-06-23 12:14:05 -0400

    added code for rest api crud operations

--- src/main/java/io/reactivestax/employeetest/service/EmployeeService.java
+++ src/main/java/io/reactivestax/employeetest/service/EmployeeService.java
@@ -2,15 +2,15 @@ package io.reactivestax.employeetest.service;
 
 import io.reactivestax.employeetest.entity.Employee;
 import io.reactivestax.employeetest.repository.EmployeeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
-import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class EmployeeService {
 
     @Autowired
@@ -21,6 +21,7 @@ public class EmployeeService {
     }
 
     public Optional<Employee> findEmployeeById(Long id) {
+        log.info("Getting Employee details using ID");
         Optional<Employee> employeeId = employeeRepository.findById(id);
         if (employeeId.isEmpty()) {
             throw new RuntimeException("Employee not found for id: " + employeeId);
@@ -29,6 +30,7 @@ public class EmployeeService {
     }
 
     public void deleteEmployee(Long id) {
+        log.info("Deleting Employee using ID");
         Optional<Employee> employeeId = employeeRepository.findById(id);
         if (employeeId.isEmpty()) {
             throw new RuntimeException("Employee not found for id: " + employeeId);
@@ -38,22 +40,24 @@ public class EmployeeService {
     }
 
     public Employee saveEmployee(Employee employee) {
+        log.info("Saving new employee");
         return employeeRepository.save(employee);
     }
 
     public Employee updateEmployee(Long id, Employee employee) {
+        log.info("Update Employee using ID");
         Optional<Employee> optionalEmployee = employeeRepository.findById(id);
         Employee employee1 = optionalEmployee.get();
         if (optionalEmployee.isPresent()) {
             employee1.setName(employee.getName());
             employee1.setPostalCode(employee.getPostalCode());
             employee1.setSalary(employee.getSalary());
-
         }
         return employeeRepository.save(employee1);
     }
 
     public Employee updateEmployeePartially(Long id, String name) {
+        log.info("updating partial information of Employee");
         Optional<Employee> optionalEmployee = employeeRepository.findById(id);
         Employee employee1 = optionalEmployee.get();
         if (optionalEmployee.isPresent()) {
