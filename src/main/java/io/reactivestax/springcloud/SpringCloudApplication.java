9fe6728c16ea03e9b3462e09f607c519aad1bf63
Maninder416 <Maninder.singh@fullstacklabs.ca>
2023-07-26 11:34:03 -0400

    added code for rest api crud operations

---
+++ src/main/java/io/reactivestax/springcloud/SpringCloudApplication.java
@@ -0,0 +1,13 @@
+package io.reactivestax.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudApplication.class, args);
+	}
+
+}
