6ec600324b9afa0db90d144be0465f7d1d293a6c
Maninder416 <Maninder.singh@fullstacklabs.ca>
2023-07-27 14:31:37 -0400

    added config server

--- order-service/src/main/java/io/reactivestax/controller/OrderController.java
+++ order-service/src/main/java/io/reactivestax/controller/OrderController.java
@@ -3,6 +3,7 @@ package io.reactivestax.controller;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -11,11 +12,15 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class OrderController {
 
+    @Value("${server.port}")
+    private String port;
+
     @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/order-payment")
     public String invokePaymentService() {
+        log.info("***** port is: *****: "+port);
         String url = "http://PAYMENT-SERVICE/payment";
         log.info("****** Invoking payment method ******");
         return restTemplate.getForObject(url, String.class);
