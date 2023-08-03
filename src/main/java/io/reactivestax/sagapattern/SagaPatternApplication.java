8bc4d7902b013cc265233184fd9e9d542d318a69
Maninder416 <Maninder.singh@fullstacklabs.ca>
2023-08-03 13:15:13 -0400

    added more content

---
+++ src/main/java/io/reactivestax/sagapattern/SagaPatternApplication.java
@@ -0,0 +1,13 @@
+package io.reactivestax.sagapattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagaPatternApplication.class, args);
+	}
+
+}
