7c5afb0f1266ce1b8016f015503e9cca97d495b9
Maninder416 <Maninder.singh@fullstacklabs.ca>
2023-08-22 10:46:39 -0400

    added more content

---
+++ src/main/java/io/reactivestax/httpsecurity/HttpSecurityApplication.java
@@ -0,0 +1,13 @@
+package io.reactivestax.httpsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/reactivestax/httpsecurity/HttpSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reactivestax.httpsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
