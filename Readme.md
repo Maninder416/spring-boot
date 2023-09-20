3b8af8b737aa4f51c8f3a8488208d8642e05b4bc
Maninder416 <Maninder.singh@fullstacklabs.ca>
2023-09-20 14:44:06 -0400

    added spring boot with mongo db

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Spring boot app with Mongo DB:
+
+```shell
+Steps to run it:
+1. Added docker-compose file contains 2 containers. One is for mongo db and another
+is for docker express for portal where you can see the mongo db portal.
+2. 
+
+```
---
+++ src/main/java/io/reactivestax/springmongo/SpringMongoApplication.java
@@ -0,0 +1,13 @@
+package io.reactivestax.springmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoApplication.class, args);
+	}
+
+}
