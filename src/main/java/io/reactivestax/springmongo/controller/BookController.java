c0ea94e9bea4da80331e941ca3665acc26309186
Maninder416 <Maninder.singh@fullstacklabs.ca>
2023-09-21 10:52:30 -0400

    added working code

---
+++ src/main/java/io/reactivestax/springmongo/controller/BookController.java
@@ -0,0 +1,40 @@
+package io.reactivestax.springmongo.controller;
+
+import io.reactivestax.springmongo.model.Book;
+import io.reactivestax.springmongo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @PostMapping("/books")
+    public String saveBook(@RequestBody Book book){
+        bookRepository.save(book);
+        return "Added book successfully";
+    }
+
+    @GetMapping("/books")
+    public List<Book> getBooks(){
+        return bookRepository.findAll();
+    }
+
+    @GetMapping("/books/{id}")
+    public Optional<Book> findBookById(@PathVariable Integer id){
+        return bookRepository.findById(id);
+    }
+
+    @DeleteMapping("/books/{id}")
+    public String deleteBook(@PathVariable int id){
+        bookRepository.deleteById(id);
+        return "Book deleted with id : "+id;
+    }
+
+
+}
---
+++ src/main/java/io/reactivestax/springmongo/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package io.reactivestax.springmongo.repository;
+
+import io.reactivestax.springmongo.model.Book;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BookRepository extends MongoRepository<Book, Integer> {
+}
