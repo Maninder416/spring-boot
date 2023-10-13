f9ad00b874b18f52892b96596a1d1d63353f7e37
Maninder416 <89317036+Maninder416@users.noreply.github.com>
2023-10-13 10:57:58 -0400

    Update Application.java

--- src/main/java/io/reactivestax/springboot/Application.java
+++ src/main/java/io/reactivestax/springboot/Application.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	@GetMapping("/")
+    	public String get(){
+        	return "hello team. I am deploying application";
+    		}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
