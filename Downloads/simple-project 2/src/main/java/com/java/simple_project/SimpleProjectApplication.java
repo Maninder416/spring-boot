e5ac40dd5bf382eace830c18d0add7a8a31f5ad9
Maninder416 <Maninder.singh@fullstacklabs.ca>
2026-08-13 11:05:28 -0400

    simple java spring boot project

---
+++ Downloads/simple-project 2/src/main/java/com/java/simple_project/SimpleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.java.simple_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleProjectApplication.class, args);
+	}
+
+}
